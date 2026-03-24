9785437f6f1b4e1d8531db69ca04c38b958e7045
kazij317-code <kazij317@gmail.com>
2026-03-24 12:59:27 +0600

    first commit

---
+++ react-world-on-the-go/src/App.jsx
@@ -0,0 +1,21 @@
+
+// --------------------------------Start: 33.1 ---------------------------------------
+
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+ 
+  return (
+    <> 
+    {/* <h1>React World on the go...</h1> */}
+    {/* (3)      commit previous and Count + enter*/}
+    <Countries></Countries>
+    </>
+  )
+}
+
+export default App
+
+
+// --------------------------------End: 33.1 ---------------------------------------
---
+++ react-world-on-the-go/src/components/Countries/Countries.jsx
@@ -0,0 +1,18 @@
+// --------------------------------Start: 33.1 ---------------------------------------
+
+// (1) make component folder then inside it Countries Folder and in this Countries.jsx  file. Click rsc and tab then in App.jsx file
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            {/* (2) */}
+            <h1>In the countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
+
+// --------------------------------End: 33.1 ---------------------------------------
